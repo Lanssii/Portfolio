aae6a0a68ba6f496493c3dd1d24d88ee1b59344a
Lana Shotashvili <shotashvililana@gmail.com>
2026-08-28 17:14:37 +0400

    refactor(components): move Card to ui directory

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -4,7 +4,7 @@ import { useState, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Card } from "./Card";
+import { Card } from "./ui/Card";
 
 gsap.registerPlugin(ScrollTrigger);
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Card, type CardData } from "./Card";
+import { Card, type CardData } from "./ui/Card";
 
 gsap.registerPlugin(ScrollTrigger);
 
